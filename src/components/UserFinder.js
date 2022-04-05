d400eb8c810b1d69f4de787cf4c14bfcc3594fcc
Alan Pecenkovic <alanquad@yahoo.com>
2022-04-05 11:46:06 +0200

    Using context with class components

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,40 +1,37 @@
 import { Fragment, useState, useEffect, Component } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: "",
     };
+  }
+
+  componentDidMount() {
+    //Send http request...
+    this.setState({ filteredUsers: this.context.users });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
   }
 
   searchChangeHandler(event) {
     this.setState({
       searchTerm: event.target.value,
     });
-  }
-
-  componentDidMount() {
-    //Send http request...
-    this.setState({ filteredUsers: DUMMY_USERS });
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchTerm !== this.state.searchTerm) {
-      this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
-          user.name.includes(this.state.searchTerm)
-        ),
-      });
-    }
   }
 
   render() {
